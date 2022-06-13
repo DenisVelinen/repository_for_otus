aed9723911114fc16983ee19692cc0d7dbffbdb2
Denis <denisvsychev@gmail.com>
2022-06-13 16:47:37 +0300

    The seventh block of tasks from the preparatory part
    Tasks have been completed https://cdn.otus.ru/media/private/1b/31/Домашнее_задание__7-68729-1b3175.pdf?hash=Pioldmpk2z97fAIFe2Mhpw&expires=1655144894

---
+++ Introduction/7_DOMHandling/hw7.js
@@ -0,0 +1,46 @@
+const buttonSendText = document.querySelector('.buttonSendText');
+const userInputText = document.querySelector('.userInputText')
+
+buttonSendText.addEventListener('click', clickOnSendButton);
+userInputText.addEventListener('input', showButton);
+
+function clickOnSendButton() {
+  const userText = getTextFromInput();
+  addParagraph(userText);
+  clearInput();
+  hideButton();
+  removeParagraph();
+}
+
+function getTextFromInput() {
+  const userText = document.querySelector('.userInputText');
+  return userText.value;
+}
+
+function addParagraph(text) {
+  const paragraph = document.createElement('p');
+  paragraph.innerText = text;
+
+  const content = document.querySelector('.content');
+  content.append(paragraph);
+}
+
+function clearInput() {
+  const input = document.querySelector('.userInputText');
+  input.value = '';
+}
+
+function showButton() {
+  buttonSendText.hidden = false;
+}
+
+function hideButton() {
+  buttonSendText.hidden = true;
+}
+
+function removeParagraph() {
+  const allParagraphs = document.querySelectorAll('p');
+  if (allParagraphs.length > 5) {
+    allParagraphs[0].remove();
+  }
+}
